46fdee3141a53a2f45755c3f5544f8268c977f9b
Raineer <delaritaraineer81@gmail.com>
2018-02-07 18:34:46 +0800

    formGroup

---
+++ src/pages/add-note/add-note.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NoteService } from '../../providers/note-service/note-service';
+import { Note } from '../../models/note.model';
+
+@IonicPage()
+@Component({
+  selector: 'page-add-note',
+  templateUrl: 'add-note.html',
+})
+export class AddNotePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private noteService: NoteService) {
+  }
+  saveNote(note: Note){
+    this.noteService.saveNote(note);
+  }
+
+}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AddNotePage } from "../add-note/add-note";
+import { NoteService } from "../../providers/note-service/note-service";
+@Component({
+  selector: "page-home",
+  templateUrl: "home.html"
+})
+export class HomePage {
+  notes: { title: string }[] = []
+  constructor(
+    public navCtrl: NavController,
+    private noteService: NoteService
+  ) {}
+  ionViewWillEnter(){
+    this.notes = this.getAllNotes();
+  }
+  addNote() {
+    this.navCtrl.push(AddNotePage);
+  }
+  getAllNotes() {
+    return this.noteService.getAllNotes();
+  }
+}
