0e7e59021951eddb676f5dbb02ebdaee21ac448c
Raineer <delaritaraineer81@gmail.com>
2018-02-08 12:56:44 +0800

    page view-note

--- src/pages/add-note/add-note.ts
+++ src/pages/add-note/add-note.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NoteService } from '../../providers/note-service/note-service';
 import { Note } from '../../models/note.model';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms/src/form_builder';
 
 @IonicPage()
 @Component({
@@ -9,11 +11,22 @@ import { Note } from '../../models/note.model';
   templateUrl: 'add-note.html',
 })
 export class AddNotePage {
+  formGroup: FormGroup;
+  note: Note;
+  date: Date = new Date();
+  title: string = '';
+  content: string = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private noteService: NoteService) {
+  constructor(public navCtrl: NavController, private noteService: NoteService) {
+    this.formGroup = new FormGroup({
+      title: new FormControl(),
+      content: new FormControl(),
+      date: new FormControl(),
+    })
   }
   saveNote(note: Note){
     this.noteService.saveNote(note);
+    this.navCtrl.pop();
   }
 
 }
